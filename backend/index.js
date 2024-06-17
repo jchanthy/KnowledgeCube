d2fa5bfed7abe953218ee8a3571a93e7b5f9353a
U-ser <john.chanthy@gmail.com>
2024-06-17 23:01:56 +0700

    init database

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { config } from "dotenv";
+import cors from "cors";
+config();
+
+const app = express();
+
+// Use the CORS middleware before defining your routes
+app.use(cors());
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to MongoDB
+// mongoose.Promise = global.Promise;
+// connect(process.env.DB_URI);
+// const db = connection;
+// db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// db.once("open", () => {
+// 	console.log("Connected to MongoDB");
+// });
+
+// connection.on("error", (err) => {
+// 	console.error("MongoDB connection error:", err);
+// });
+
+// Routes
+import apiRoutes from "./routes/api.js"; // Import your API routes
+app.use("/api", apiRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Something went wrong!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
---
+++ backend/routes/api.js
@@ -0,0 +1,9 @@
+// api.js
+import { Router } from "express";
+const router = Router();
+
+import userRoutes from "./userRoutes.js";
+
+router.use("/users", userRoutes);
+
+export default router;
